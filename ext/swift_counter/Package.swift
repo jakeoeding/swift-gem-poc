e10e280a950dcdc197da9ad09c2cfb9407aac22c
jake.oeding <jake.oeding@instructure.com>
2026-03-09 11:14:30 -0400

    add linux compatibility

--- ext/swift_counter/Package.swift
+++ ext/swift_counter/Package.swift
@@ -7,7 +7,7 @@ let package = Package(
     products: [
         .library(
             name: "Counter",
-            type: .static,
+            type: .dynamic,
             targets: ["Counter"]
         ),
     ],
--- ext/swift_counter/extconf.rb
+++ ext/swift_counter/extconf.rb
@@ -1,8 +1,10 @@
 require "mkmf"
 
-system("swift build -c release --static-swift-stdlib")
+system("swift build -c release")
+
+swift_lib_dir = File.expand_path(".build/release")
 
 $CFLAGS << " -O3 -Wall"
-$LDFLAGS << " ./.build/release/libCounter.a"
+$LDFLAGS << " -Wl,-rpath,#{swift_lib_dir} -L./.build/release -lCounter"
 
 create_makefile("swift_counter/swift_counter")
