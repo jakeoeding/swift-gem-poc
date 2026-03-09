38f4760f952bf5bf3c6e08647619e276ab3f7c55
jake.oeding <jake.oeding@instructure.com>
2026-03-09 15:54:43 -0400

    fix incorrect return types in c header

--- ext/swift_counter/counter.h
+++ ext/swift_counter/counter.h
@@ -8,9 +8,9 @@ typedef struct Counter Counter;
 Counter* counter_create(int32_t initial_count, int32_t step);
 int32_t counter_increment(Counter *counter);
 int32_t counter_get_count(Counter *counter);
-int32_t counter_set_count(Counter *counter, int32_t new_count);
+void counter_set_count(Counter *counter, int32_t new_count);
 int32_t counter_get_step(Counter *counter);
-int32_t counter_set_step(Counter *counter, int32_t new_step);
+void counter_set_step(Counter *counter, int32_t new_step);
 void counter_destroy(Counter *counter);
 
 #endif // COUNTER_H
