e743f81fe731239b9f90a03fab5d0eed9df28345
jake.oeding <jake.oeding@instructure.com>
2026-05-21 15:54:18 -0400

    upgrade to swift 6.3

--- ext/swift_counter/Sources/Counter/CounterBridge.swift
+++ ext/swift_counter/Sources/Counter/CounterBridge.swift
@@ -1,40 +1,40 @@
-@_cdecl("counter_create")
+@c(counter_create)
 public func counterCreate(initialCount: Int32, step: Int32) -> UnsafeMutableRawPointer {
     let counter = Counter(initialCount: initialCount, step: step)
     return Unmanaged.passRetained(counter).toOpaque()
 }
 
-@_cdecl("counter_increment")
+@c(counter_increment)
 public func counterIncrement(ptr: UnsafeMutableRawPointer) -> Int32 {
     let counter = Unmanaged<Counter>.fromOpaque(ptr).takeUnretainedValue()
     return counter.increment()
 }
 
-@_cdecl("counter_get_count")
+@c(counter_get_count)
 public func counterGetCount(ptr: UnsafeMutableRawPointer) -> Int32 {
     let counter = Unmanaged<Counter>.fromOpaque(ptr).takeUnretainedValue()
     return counter.count
 }
 
-@_cdecl("counter_set_count")
+@c(counter_set_count)
 public func counterSetCount(ptr: UnsafeMutableRawPointer, newCount: Int32) {
     let counter = Unmanaged<Counter>.fromOpaque(ptr).takeUnretainedValue()
     counter.count = newCount
 }
 
-@_cdecl("counter_get_step")
+@c(counter_get_step)
 public func counterGetStep(ptr: UnsafeMutableRawPointer) -> Int32 {
     let counter = Unmanaged<Counter>.fromOpaque(ptr).takeUnretainedValue()
     return counter.step
 }
 
-@_cdecl("counter_set_step")
+@c(counter_set_step)
 public func counterSetStep(ptr: UnsafeMutableRawPointer, newStep: Int32) {
     let counter = Unmanaged<Counter>.fromOpaque(ptr).takeUnretainedValue()
     counter.step = newStep
 }
 
-@_cdecl("counter_destroy")
+@c(counter_destroy)
 public func counterDestroy(ptr: UnsafeMutableRawPointer) {
     Unmanaged<Counter>.fromOpaque(ptr).release()
 }
